b7213b15587b9095c0d52880899920a34a22d1e0
priyanshujalan <67287768+priyanshujalan@users.noreply.github.com>
2021-11-24 22:47:03 +0545

    Updated login.js!
    Updated the Doit() function

--- public/js/login.js
+++ public/js/login.js
@@ -17,24 +17,4 @@ function doIt(){
     minHand.style.transform = "rotate("+(6 * minPast)+"deg)";
     secHand.style.transform = "rotate("+(6 * secPast)+"deg)";
 
-    if(hoursPast>=5 && hoursPast<12){
-
-        text.innerHTML = "Good Morning";
-    }
-
-    else if(hoursPast>=12 && hoursPast<16){
-
-        text.innerHTML = "Good Afternoon";
-    }
-
-    else if(hoursPast>=16 && hoursPast<22){
-
-        text.innerHTML = "Good Evening";
-    }
-
-    else if(hoursPast>=22 || hoursPast<5){
-
-        text.innerHTML = "So jaa bhai, kuch nhi hoga uth ke. Good Night";
-    }
-
 }
